42e6a9c52af8136edb0c9637de329bdacccacf52
Aadith <2004aadith@gmail.com>
2023-03-14 16:34:13 +0530

    slight changes

--- Server/OTP.js
+++ Server/OTP.js
@@ -1,7 +1,7 @@
 module.exports = {
-    serviceID:"VAbfc2fe81ab2f742512a24419bb8b0cbe",
-    accountSID:"ACe17cb0c64eb13df1b90f60fd96a17515",
-    authToken:"9bf9e229d3b74267c943f91f73ac3a16"
+    serviceID:process.env.serviceID,
+    accountSID:process.env.accountSID,
+    authToken:process.env.authToken
 }
 
 
--- Server/app.js
+++ Server/app.js
@@ -1,18 +1,14 @@
-const express = require('express');
-const { default: mongoose } = require('mongoose');
-
+const express = require("express");
+const { default: mongoose } = require("mongoose");
+const dotenv = require("dotenv");
 const app = express();
+dotenv.config();
 
-const homeRouter = require("./Routes/router")
-mongoose.connect(
-    "mongodb+srv://aadith:1234@cluster0.zc9nm2l.mongodb.net/?retryWrites=true&w=majority"
-  );
-app.use(express.json())
-app.use("/",homeRouter)
+const homeRouter = require("./Routes/router");
+mongoose.connect(process.env.mongoose);
+app.use(express.json());
+app.use("/", homeRouter);
 
-
-
-
-app.listen(3000,()=>{
-    console.log('Server running port on http::localhost:3000');
-})
+app.listen(3000, () => {
+  console.log("Server running port on http::localhost:3000");
+});
